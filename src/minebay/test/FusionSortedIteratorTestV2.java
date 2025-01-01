d97a8834ec76a99a08cefe53cfb24ea60498bd3e
youcefIDE <gghedamsi.youcef@gmail.com>
2025-01-02 00:04:39 +0100

    update

--- src/minebay/test/FusionSortedIteratorTestV2.java
+++ src/minebay/test/FusionSortedIteratorTestV2.java
@@ -1,94 +1,201 @@
 package minebay.test;
 
-import minebay.AdCategory;
-import minebay.ClassifiedAd;
-
 import minebay.FusionSortedIterator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
-public class FusionSortedIteratorTestV2 {
+import static org.junit.jupiter.api.Assertions.*;
 
-        private ListIterator<Integer> iterator1;
-        private ListIterator<Integer> iterator2;
-        private ListIterator<Integer> iterator3;
+/**
+ * Classe de test pour FusionSortedIterator
+ * @author Youcef
+ */
+class FusionSortedIteratorTestV2 {
+
+        private ListIterator<Integer> it1, it2, it3;
+        private FusionSortedIterator<Integer> iterator;
 
         @BeforeEach
-        public void setUp() {
-                iterator1 = new ArrayList<>(Arrays.asList(1, 4, 7)).listIterator();
-                iterator2 = new ArrayList<>(Arrays.asList(2, 5, 8)).listIterator();
-                iterator3 = new ArrayList<>(Arrays.asList(3, 6, 9)).listIterator();
+        void setUp() {
+                it1 = new ArrayList<>(Arrays.asList(1, 3, 5)).listIterator();
+                it2 = new ArrayList<>(Arrays.asList(2, 4, 6)).listIterator();
+                it3 = new ArrayList<>(Arrays.asList(0, 7, 8)).listIterator();
         }
 
         @Test
-        public void testHasNextAndNext() {
-                FusionSortedIterator<Integer> fusionIterator = new FusionSortedIterator<>(
-                        Arrays.asList(iterator1, iterator2, iterator3)
-                );
+        void testConstructorWithNaturalOrder() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2, it3));
+                assertNotNull(iterator.comparator());
+                assertTrue(iterator.hasNext());
+                assertFalse(iterator.hasPrevious());
+        }
 
-                ArrayList<Integer> result = new ArrayList<>();
-                while (fusionIterator.hasNext()) {
-                        result.add(fusionIterator.next());
+        @Test
+        void testConstructorWithCustomComparator() {
+                Comparator<Integer> reverseOrder = Comparator.reverseOrder();
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2), reverseOrder);
+                assertEquals(reverseOrder, iterator.comparator());
+        }
+
+        @Test
+        void testHasNext() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertTrue(iterator.hasNext());
+                while (iterator.hasNext()) {
+                        iterator.next();
                 }
-
-                assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9), result);
+                assertFalse(iterator.hasNext());
         }
 
         @Test
-        public void testHasPreviousAndPrevious() {
-                FusionSortedIterator<Integer> fusionIterator = new FusionSortedIterator<>(
-                        Arrays.asList(iterator1, iterator2, iterator3)
-                );
+        void testNext() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertEquals(1, iterator.next());
+                assertEquals(2, iterator.next());
+                assertEquals(3, iterator.next());
+        }
 
-                // Traverse forward
-                while (fusionIterator.hasNext()) {
-                        fusionIterator.next();
+        @Test
+        void testHasPrevious() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertFalse(iterator.hasPrevious());
+                iterator.next();
+                assertTrue(iterator.hasPrevious());
+        }
+
+        @Test
+        void testPrevious() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2, it3));
+                iterator.next();
+                iterator.next();
+                iterator.next();
+                assertEquals(3, iterator.previous());
+                assertEquals(2, iterator.previous());
+                assertEquals(1, iterator.previous());
+        }
+
+        @Test
+        void testPreviousOnBoundary() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                iterator.next();
+                assertEquals(1, iterator.previous());
+                assertThrows(NoSuchElementException.class, iterator::previous);
+        }
+
+        @Test
+        void testPreviousWithEmptyIterators() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(
+                        new ArrayList<Integer>().listIterator(),
+                        new ArrayList<Integer>().listIterator()
+                ));
+                assertFalse(iterator.hasPrevious());
+                assertThrows(NoSuchElementException.class, iterator::previous);
+        }
+
+        @Test
+        void testRemove() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                iterator.next();
+                iterator.remove();
+                assertFalse(it1.hasPrevious());
+                assertEquals(3, it1.next());
+        }
+
+        @Test
+        void testRemoveAfterPrevious() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                iterator.next();
+                iterator.next();
+                iterator.previous();
+                iterator.remove();
+                assertEquals(2, it2.next());
+        }
+
+        @Test
+        void testRemoveWithoutCallingNextOrPrevious() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertThrows(IllegalStateException.class, iterator::remove);
+        }
+
+        @Test
+        void testRemoveMultipleTimes() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                iterator.next();
+                iterator.remove();
+                assertThrows(IllegalStateException.class, iterator::remove);
+        }
+
+        @Test
+        void testRemoveOnEmptyIterator() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(
+                        new ArrayList<Integer>().listIterator()
+                ));
+                assertFalse(iterator.hasNext());
+                assertThrows(NoSuchElementException.class, iterator::next);
+                assertThrows(IllegalStateException.class, iterator::remove);
+        }
+
+        @Test
+        void testNextIndex() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertEquals(0, iterator.nextIndex());
+                iterator.next();
+                assertEquals(1, iterator.nextIndex());
+        }
+
+        @Test
+        void testPreviousIndex() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertEquals(-1, iterator.previousIndex());
+                iterator.next();
+                assertEquals(0, iterator.previousIndex());
+        }
+
+        @Test
+        void testLastIndex() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertEquals(-1, iterator.lastIndex());
+                iterator.next();
+                assertEquals(0, iterator.lastIndex());
+                iterator.previous();
+                assertEquals(0, iterator.lastIndex());
+        }
+
+        @Test
+        void testUnsupportedOperations() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertThrows(UnsupportedOperationException.class, () -> iterator.add(10));
+                assertThrows(UnsupportedOperationException.class, () -> iterator.set(10));
+        }
+
+        @Test
+        void testInvalidConstructorArguments() {
+                assertThrows(NullPointerException.class, () -> new FusionSortedIterator<>(null));
+                assertThrows(NullPointerException.class, () -> new FusionSortedIterator<>(Arrays.asList(it1, null)));
+                assertThrows(NullPointerException.class, () -> new FusionSortedIterator<>(Arrays.asList(it1, it2), null));
+        }
+
+        @Test
+        void testGetNextAndPrevious() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                assertEquals(1, iterator.getNext());
+                iterator.next();
+                assertEquals(1, iterator.getPrevious());
+        }
+
+        @Test
+        void testPreviousAfterExhaustingNext() {
+                iterator = new FusionSortedIterator<>(Arrays.asList(it1, it2));
+                while (iterator.hasNext()) {
+                        iterator.next();
                 }
-
-                ArrayList<Integer> result = new ArrayList<>();
-                while (fusionIterator.hasPrevious()) {
-                        result.add(fusionIterator.previous());
-                }
-
-                assertEquals(Arrays.asList(9, 8, 7, 6, 5, 4, 3, 2, 1), result);
-        }
-
-        @Test
-        public void testGetNext() {
-                FusionSortedIterator<Integer> fusionIterator = new FusionSortedIterator<>(
-                        Arrays.asList(iterator1, iterator2, iterator3)
-                );
-
-                assertEquals(1, fusionIterator.getNext());
-                assertEquals(1, fusionIterator.getNext()); // Should not move the cursor
-        }
-
-        @Test
-        public void testGetPrevious() {
-                FusionSortedIterator<Integer> fusionIterator = new FusionSortedIterator<>(
-                        Arrays.asList(iterator1, iterator2, iterator3)
-                );
-
-                // Traverse forward
-                while (fusionIterator.hasNext()) {
-                        fusionIterator.next();
-                }
-
-                assertEquals(9, fusionIterator.getPrevious());
-                assertEquals(9, fusionIterator.getPrevious()); // Should not move the cursor
-        }
-
-        @Test
-        public void testComparator() {
-                FusionSortedIterator<Integer> fusionIterator = new FusionSortedIterator<>(
-                        Arrays.asList(iterator1, iterator2, iterator3)
-                );
-
-                assertNotNull(fusionIterator.comparator());
+                assertTrue(iterator.hasPrevious());
+                assertEquals(6, iterator.previous());
         }
 }
